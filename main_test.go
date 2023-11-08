ada7e1bb9d119a711ce3a108d86b25f0a1ce4f79
Sven Hohlfeld <sho1975@gmail.com>
2023-11-08 22:33:26 +0100

    commit to help with twitter https://twitter.com/rebiiin/status/1722317893096874229

--- main_test.go
+++ main_test.go
@@ -1,20 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-// GORM_REPO: https://github.com/go-gorm/gorm.git
-// GORM_BRANCH: master
-// TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
-
-func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
-
+// TestTwitter1722317893096874229 explains https://gorm.io/docs/query.html#Selecting-Specific-Fields
+func TestTwitter1722317893096874229(t *testing.T) {
+	var users []User
+	user := User{Name: "jinzhu", Age: 99, Active: true}
 	DB.Create(&user)
+	defer DB.Delete(&user)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+	result := DB.Select([]string{"Name", "Active"}).Find(&users)
+
+	if result.Error != nil {
+		t.Fatalf("Epected user not found. Got error: %v", result.Error)
 	}
+
+	fmt.Printf("Users found: %v", users)
+}
+
+// TestTwitter1722317893096874229ResponseStruct explains https://gorm.io/docs/advanced_query.html
+func TestTwitter1722317893096874229ResponseStruct(t *testing.T) {
+
+	type ResponseUser struct {
+		Name   string `json:"ResponseName"`
+		Active string `json:"ResponseActive"`
+	}
+	var responseUsers []ResponseUser
+	user := User{Name: "jinzhu", Age: 99, Active: true}
+	DB.Create(&user)
+	defer DB.Delete(&user)
+
+	result := DB.Model(&User{}).Select([]string{"Name", "Active"}).Find(&responseUsers)
+
+	if result.Error != nil {
+		t.Fatalf("Epected user not found. Got error: %v", result.Error)
+	}
+
+	fmt.Printf("Users found: %v", responseUsers)
 }
